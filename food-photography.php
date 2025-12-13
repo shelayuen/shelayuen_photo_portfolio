ad1de88fe0e0a8ec69a899b5fdc9dd9f88178934
shelayuen <151683991+shelayuen@users.noreply.github.com>
2025-12-12 22:40:26 -0800

    conso footer

--- food-photography.php
+++ food-photography.php
@@ -113,76 +113,7 @@
 		</div>
 		<!-- page-content -->
 		<!-- /PAGE CONTENT -->
-		<div id="footer-instagram">
-			<ul class="instagram-pics instagram-size-small">
-			<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-1.png" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-2.png" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-3.png" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-4.png" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-5.jpg" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-6.jpg" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-7.png" alt=""/></a></li>
-				<li class=""><a href="https://instagram.com/shelayuen" target="_blank" class=""><img src="images/insta/insta-8.png" alt=""/></a></li>
-			</ul>
-			<p class="clear"><a href="#" rel="me" target="_blank" class="">Follow Us!</a></p>
-		</div>
-		<!-- FOOTER -->
-		<footer>
-			<div class="container">
-				<div class="footer-widgets">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="foo-block">
-								<div class="widget widget-footer widget_text">
-									<h5 class="widgettitle">About Me</h5>
-									<div class="textwidget">
-										<p>Marketing Manager & Digital Designer for Luxury Hotel. Shela combines her years of award-winning sales experience and her talent in expressing complicated concepts through visuals, to help premium hotel market themselves digitally.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="foo-block">
-								<div class="widget widget-footer widget_text">
-									<h5 class="widgettitle">Contact</h5>
-									<div class="textwidget">
-										<div class="textwidget">
-											<p>Based in Vancouver<br> British Columbia, Canada</p>
-											<p>info@shelayuen.site</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="foo-block foo-last">
-								<div class="widget_text widget widget-footer widget_custom_html">
-									<h5 class="widgettitle">Quick Links</h5>
-									<div class="textwidget custom-html-widget">
-										<ul>
-											<li> <a href="index.html">Home</a></li>
-											<li><a href="about-me.html">About</a></li>
-											<li><a href="#">Portfolio</a></li>
-											<li><a href="success-stories.html">Success Stories</a></li>
-											<li><a href="contact.html">Contact</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="copyright">
-					<div class="footer-copy"> Copyright &copy; 2025 Shela Yuen</div>
-				</div>
-			</div>
-		</footer>
-		<!-- FOOTER -->
-		<div class="scrollup">
-			<a class="scrolltop" href="#">
-			<i class="fa fa-chevron-up"></i>
-			</a>
-		</div>
+		<?php include 'footer.php'; ?>
 		<!-- JS --> 
 		<script src='js/jquery.js'></script>
 		<script src='js/jquery-migrate.min.js'></script>
