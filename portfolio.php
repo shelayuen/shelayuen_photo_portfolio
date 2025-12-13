0abf4d18b681a895065fb246630f74663a5bc87c
shelayuen <151683991+shelayuen@users.noreply.github.com>
2025-12-13 11:22:30 -0800

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Shela Yuen | Marketing Manager and Digital Designer for Premium Hotels</title>
+		<!-- Google Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,500;0,600;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,700&display=swap" rel="stylesheet">
+		<!-- Bootstrap CSS -->
+		<link rel='stylesheet' id='bootstrap-css'  href='css/bootstrap/css/bootstrap.css' type='text/css' media='all' />
+		<!-- Font Awesome Icons CSS -->
+		<link rel='stylesheet' id='font-awesome'  href='css/fontawesome/css/all.css' type='text/css' media='all' />
+		<!-- Owl Carousel -->
+		<link rel='stylesheet' id='owl-carousel'  href='js/owl-carousel/owl.carousel.css' type='text/css' media='all' />
+		<!-- Main CSS File -->
+		<link rel='stylesheet' id='gleam-style-css'  href='style.css' type='text/css' media='all' />
+		<!-- favicons -->
+		<link rel="icon" href="images/icons/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" href="images/icons/favicon-192x192.png" sizes="192x192" />
+		<link rel="apple-touch-icon-precomposed" href="images/icons/favicon-180x180.png" />
+	</head>
+	<body class="home">
+		<div class="menu-mask"></div>
+		<?php include 'header.php'; ?>
+		<!-- PAGE CONTENT -->
+		<div class="page-holder custom-page-template page-full fullscreen-page home-page-content clearfix">
+			<!-- SECTION 1-->
+			<section class="parallax section-holder section-home41" style="background-image:url('images/albums/up.jpg');">
+				<div class="container parallax-content alignc">
+					<div class="row">
+						<div class="col-lg-8 offset-lg-2">
+							<h1 class="display-3 white">Digital storytelling for premium hotels.</h1>
+						</div>
+					</div>
+					<!-- /row -->			
+				</div>
+				<!-- /container -->
+			</section>
+			<!-- /SECTION 1-->
+
+			<!-- SECTION 2-->
+			<section class="parallax section-holder section-home41" style="background-image:url('images/albums/up.jpg');">
+				<div class="container parallax-content alignc">
+					<div class="row">
+						<div class="col-lg-8 offset-lg-2">
+							<h1 class="display-3 white">Digital storytelling for premium hotels.</h1>
+						</div>
+					</div>
+					<!-- /row -->			
+				</div>
+				<!-- /container -->
+			</section>
+			<!-- /SECTION 2-->
+
+			<!-- SECTION 3-->
+			<section class="parallax section-holder section-home41" style="background-image:url('images/albums/up.jpg');">
+				<div class="container parallax-content alignc">
+					<div class="row">
+						<div class="col-lg-8 offset-lg-2">
+							<h1 class="display-3 white">Digital storytelling for premium hotels.</h1>
+						</div>
+					</div>
+					<!-- /row -->			
+				</div>
+				<!-- /container -->
+			</section>
+			<!-- /SECTION 3-->
+		</div>
+		<!-- home-page-content -->
+		<!-- /PAGE CONTENT -->
+		<?php include 'footer.php'; ?>
+		<!-- JS --> 
+		<script src='js/jquery.js'></script>
+		<script src='js/jquery-migrate.min.js'></script>
+		<script src='css/bootstrap/js/popper.js'></script>
+		<script src='css/bootstrap/js/bootstrap.js'></script>
+		<script src='js/easing.js'></script>
+		<script src='js/fitvids.js'></script>
+		<script src='js/owl-carousel/owl.carousel.js'></script>
+		<script src='js/isotope.js'></script>
+		<script src='js/simple-lightbox.js'></script>
+		<!-- MAIN JS -->
+		<script src='js/init.js'></script>
+
+		<!-- for filtering -->
+    	<script src="js/filtering.js"></script>
+
+		<!-- CONTACT FORM JS -->
+		<script src='js/jquery.form.min.js'></script>
+		<script src='js/contactform.js'></script>
+		
+	</body>
+</html>
