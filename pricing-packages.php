b14bad1dedd9dc662cdcd72431214a8f7fd62d9c
shelayuen <151683991+shelayuen@users.noreply.github.com>
2025-12-23 12:28:14 -0800

    Update pricing-packages.php

--- pricing-packages.php
+++ pricing-packages.php
@@ -288,7 +288,7 @@
 							</div>	
 							<!-- /row -->	
 						</div>
-						
+
 					</div>
 					<!-- /row -->
 
@@ -296,22 +296,12 @@
 				<!-- /container -->
 			</section>
 			<!-- /SECTION 1-->
-			<!-- SECTION 2-->
-			<section>
-				<div class="gmaps">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.9762977047603!2d-73.96009086693977!3d40.69651965846399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25bc1f922373f%3A0x398427d98fba822e!2sPark+Ave%2C+Brooklyn%2C+NY%2C+USA!5e0!3m2!1sen!2sro!4v1471514814808">
-					</iframe>
-				</div>
-			</section>
-			<!-- /SECTION 2-->
+
 		</div>
-		<!-- page-content -->
+
 		<!-- /PAGE CONTENT -->
-	
-		
 		</div>
-		<!-- page-content -->
-		<!-- /PAGE CONTENT -->
+
 		<?php include 'footer.php'; ?>
 		<!-- JS --> 
 		<script src='js/jquery.js'></script>
