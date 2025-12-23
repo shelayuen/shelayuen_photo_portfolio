1766391fc2695ad2eb3153d5a2f9ce68d6e80f93
shelayuen <151683991+shelayuen@users.noreply.github.com>
2025-12-23 10:19:42 -0800

    add google tag

--- blog-single-post.php
+++ blog-single-post.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-47LJXC4ZD9"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-47LJXC4ZD9');
+		</script>
+		
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Blog | Shela Yuen, Markerting Manager & Digital Designer</title>
--- social-media-management.php
+++ social-media-management.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-47LJXC4ZD9"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-47LJXC4ZD9');
+		</script>
+		
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Social Media Management | Shela Yuen | Marketing Manager and Digital Designer for Premium Hotels</title>
