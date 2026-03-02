e3c92f1ba05d0811c9ddae22f9e300938be9c09d
Shela Yuen <151683991+shelayuen@users.noreply.github.com>
2026-03-02 14:03:21 -0800

    Create 2026valentine.php

---
+++ 2026valentine.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-47LJXC4ZD9"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-47LJXC4ZD9');
+		</script>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Blog | Shela Yuen, Markerting Manager & Digital Designer</title>
+
+		<!-- Open Graph Metatags -->
+		<meta property="og:title" content="Shela Yuen | Marketing Manager and Digital Designer for Premium Hotels" />
+		<meta property="og:description" content="Shela Yuen | Marketing Manager and Digital Designer for Premium Hotels">
+		<meta property="og:image" content="https://shelayuen.site/images/team/opengraph.png" />
+		<meta property="og:url" content="https://shelayuen.site/" />
+		<meta property="og:type" content="website">
+		<meta property="og:locale" content="en_US">
+		<meta property="og:site_name" content="Shela Yuen | Marketing Manager and Digital Designer for Premium Hotels">
+
+		<!-- Google Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;0,500;0,600;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,700&display=swap" rel="stylesheet">
+		<!-- Bootstrap CSS -->
+		<link rel='stylesheet' id='bootstrap-css'  href='css/bootstrap/css/bootstrap.css' type='text/css' media='all' />
+		<!-- Font Awesome Icons CSS -->
+		<link rel='stylesheet' id='font-awesome'  href='css/fontawesome/css/all.css' type='text/css' media='all' />
+		<!-- Owl Carousel -->
+		<link rel='stylesheet' id='owl-carousel'  href='js/owl-carousel/owl.carousel.css' type='text/css' media='all' />
+		<!-- Main CSS File -->
+		<link rel='stylesheet' id='gleam-style-css'  href='style.css' type='text/css' media='all' />
+		<!-- favicons -->
+		<link rel="icon" href="images/icons/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" href="images/icons/favicon-192x192.png" sizes="192x192" />
+		<link rel="apple-touch-icon-precomposed" href="images/icons/favicon-180x180.png" />
+	</head>
+	<body>
+		<div class="menu-mask"></div>
+		<?php include 'header.php'; ?>
+		<!-- PAGE TOP -->
+		<div class="top-single-bkg single-post-topheader  topsingleimage " style="background-image:url('images/blog/mothersdayreseve/mothersdayreserve.png');">
+			<div class="inner-desc">
+				<div class="container">
+					<h1 class="post-title single-post-title">Digital Marketing Ecology - converting exposure to real bookings</h1>
+					<ul class="post-meta">
+						<li class="meta-date">March 1, 2026</li>
+						<li class="meta-categ"><a href="#" rel="category tag">Social Media</a> <a href="#" rel="category tag">Marketing</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- /PAGE TOP -->
+		<!-- PAGE CONTENT -->
+		<div id="wrap-content" class="blog-post-single section-holder">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9  posts-holder post-sidebar ">
+						<article class="blog-item-1col single-post-holder single-article-container">
+							<div class="post-content single-post-content single-article-page clearfix">
+								<p>Social media remains one of the most cost-effective marketing channels available — but only when it's used strategically. Simply posting isn't enough. The real magic happens when you create a clear pathway from social media exposure to actual bookings.</p>
+								<h2>Why Social Media Works for Hospitality<br></h2>
+								<p> If you've already got a solid brand, good offerings, and fair pricing, social media does three things really well: it gets you in front of more people, it builds trust when you show up consistently, and it reminds guests you exist when they're ready to book. You just need to make it dead simple for them to take that next step.
+								</p>
+								<div class="row">
+									<div class="col-lg-6">
+										<figure><a href="#"><img src="images/blog/mothersdayreserve/mdr-post.png" alt="" class="img-fluid" ></a></figure>
+									</div>
+									<div class="col-lg-6">
+										<figure><a href="#"><img src="images/blog/mothersdayreserve/mdr-post-2.png" alt="" class="img-fluid" ></a></figure>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="col-lg-6">
+										<figure><a href="#"><img src="images/blog/mothersdayreserve/mdr-story.png" alt="" class="img-fluid" ></a></figure>
+									</div>
+									<div class="col-lg-6">
+										<figure><a href="#"><img src="images/blog/mothersdayreserve/mdr-campaign.png" alt="" class="img-fluid" ></a></figure>
+									</div>
+								</div>
+
+								<div class="spacer-50"></div>
+
+								<h2>The Strategy: Creating a Seamless Marketing Funnel</h2>
+								<p> Instead of just throwing up some pretty posts and hoping for the best, I treated the whole thing like a funnel—guiding people from their first scroll all the way to hitting "reserve."</p>
+								<p> <b>Step 1: A Clear offer</b> First, I got clear on what we were offering: Mother's Day reservations with a special menu worth celebrating.
+								</p>
+								<!-- <blockquote class="wp-block-quote">
+									<p> The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.</p>
+									<cite>Marcel Proust</cite>
+								</blockquote> -->
+								<p> <b>Step 2: Map the User Journey</b> Then I mapped out exactly how someone would go from seeing our post to booking a table:</p>
+								<ul>
+									<li>They see our post or story, reminding them Mother's Day is coming</li>
+									<li>They get interested in seeing what is on the menu, if they still haven't booked a place for Mother's Day Brunch</li>
+									<li>They click the link in story or the link in bio, directing to the landing page</li>
+									<li>They land on a simple page with everything they need: dates, times, the menu, and a big intuitive reservation form to reserve</li>
+								</ul>
+								<div class="row">
+									<div class="col-lg-6">
+										<figure><a href="#"><img src="images/blog/mothersdayreserve/mdr-landingpage.png" alt="" class="img-fluid" ></a></figure>
+									</div>
+									<div class="col-lg-6">
+										<figure><a href="#"><img src="images/blog/mothersdayreserve/mdr-reserve-form.png" alt="" class="img-fluid" ></a></figure>
+									</div>
+								</div>
+
+								<div class="spacer-50"></div>
+
+								<h2>The Results</h2>
+								
+								<p>In two weeks, we got reservation emails flying in every time we posted something on social media. At the end we had received 300% more reservations than the week before the social campaigns, and five times our normal revenue for that month.
+								</p>
+								<h2>The Takeaway</h2>
+								<p>Social media gets people interested, but interest doesn't equal money until you make it stupid-easy to book. Every extra click or confusing step loses you customers. When we removed all the friction between "that looks nice" and "table for four, please," the bookings rolled in.
+								</p>
+							</div>
+							<div class="tags-single-page"> <a href="#" rel="tag">marketing</a> <a href="#" rel="tag">social media</a> <a href="#" rel="tag">funnels</a></div>
+							<div class="author-single-page">
+								<div class="author-avatar"> <img alt="" src="images/team/team-3.png" width="100" height="100"></div>
+								<div class="author-content">
+									<h3><a href="#">Shela Yuen</a></h3>
+									<div class="author-text">Marketing Manager & Digital Designer for Premium Hotel. Shela combines her years of award-winning sales experience and her talent in expressing complicated concepts through visuals, to help premium hotel market themselves digitally.</div>
+								</div>
+							</div>
+							<div class="row meta-nav-holder">
+								<div class="col-md-6 meta-nav">
+									<div class="pn-desc">
+										<h3><a href="contact.php">&larr; Contact Me</a></h3>
+									</div>
+								</div>
+								<div class="col-md-6 meta-nav meta-nav-right">
+									<div class="pn-desc">
+										<h3><a href="index.php">Home &rarr;</a></h3>
+									</div>
+								</div>
+							</div>
+							<!-- <section id="comments" class="comm-title">
+								<div class="respond">
+									<div id="comment-form-holder">
+										<div id="respond" class="comment-respond">
+											<h3 id="reply-title" class="comment-reply-title"><span>Leave a Comment</span> </h3>
+											<form action="#" method="post" id="commentform" class="comment-form" novalidate="">
+												<p><textarea name="comment" id="msg-contact" placeholder="Comment" rows="7" tabindex="3"></textarea></p>
+												<div class="row">
+													<div class="col-sm-4 margin-bm32"><input type="text" name="author" id="author" class="comm-field" value="" placeholder="Name" size="22" tabindex="1"></div>
+													<div class="col-sm-4 margin-bm32"><input type="text" name="email" id="email" class="comm-field" value="" placeholder="Email" size="22" tabindex="2"></div>
+													<div class="col-sm-4"><input type="text" name="url" id="url" class="comm-field" value="" placeholder="Website" size="22" tabindex="3"></div>
+												</div>
+												<p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="Post Comment"></p>
+											</form>
+										</div>
+										<div id="output-contact"></div>
+									</div>
+								</div>
+							</section> -->
+						</article>
+					</div>
+					<!-- right column - about me & recent articles -->
+					<div class="col-lg-3">
+						<aside>
+							<ul>
+								<li id="text-2" class="widget widget_text">
+									<h5 class="widgettitle">About Me</h5>
+									<div class="textwidget">
+										<p><img class="img-fluid" src="images/team/team-3.png" alt="" width="250" height="250"></p>
+										<p>Marketing Manager & Digital Designer for Premium Hotel. Shela combines her years of award-winning sales experience and her talent in expressing complicated concepts through visuals, to help premium hotel market themselves digitally.</p>
+									</div>
+								</li>
+								<li class="widget">
+									<h5 class="widgettitle">Recent Articles</h5>
+									<div class="widget-display-posts-img">
+										<ul>
+											<li>
+												<div class="display-post-holder">
+													<div class="display-post-img"> <a href="blog-single-post.php" title="Combining Social Media & Marketing funnels"><img src="images/blog/mothersdayreserve/mothersdayreserve.png" class="img-fluid" alt="Combining Social Media & Marketing funnels" title="Combining Social Media & Marketing funnels"> </a></div>
+													<div class="display-post-text">
+														<p class="display-post-date">December 20, 2025</p>
+														<div class="display-post-title"><a href="blog-single-post.php">Combining Social Media & Marketing funnels</a></div>
+													</div>
+												</div>
+											</li>
+											<!-- <li>
+												<div class="display-post-holder">
+													<div class="display-post-img"> <a href="blog-single-post.php" title="New Dress Collection"><img src="images/blog/blog-8-sm.jpg" class="img-fluid" alt="New Dress Collection" title="New Dress Collection"> </a></div>
+													<div class="display-post-text">
+														<p class="display-post-date">August 18, 2021</p>
+														<div class="display-post-title"><a href="blog-single-post.php">New Dress Collection</a></div>
+													</div>
+												</div>
+											</li>
+											<li>
+												<div class="display-post-holder">
+													<div class="display-post-img"> <a href="blog-single-post.php" title="Family Moments"><img src="images/blog/blog-2-sm.jpg" class="img-fluid" alt="Family Moments" title="Family Moments"> </a></div>
+													<div class="display-post-text">
+														<p class="display-post-date">August 16, 2021</p>
+														<div class="display-post-title"><a href="blog-single-post.php">Family Moments</a></div>
+													</div>
+												</div>
+											</li>
+											<li>
+												<div class="display-post-holder">
+													<div class="display-post-img"> <a href="blog-single-post.php" title="Red Project"><img src="images/blog/blog-6-sm.jpg" class="img-fluid" alt="Red Project" title="Red Project"> </a></div>
+													<div class="display-post-text">
+														<p class="display-post-date">August 14, 2021</p>
+														<div class="display-post-title"><a href="blog-single-post.php">Red Project</a></div>
+													</div>
+												</div>
+											</li> -->
+										</ul>
+									</div>
+								</li>
+								<li class="widget widget_categories">
+									<h5 class="widgettitle">Categories</h5>
+									<ul>
+										<li class="cat-item cat-item-4"><a href="#">Marketing</a></li>
+										<li class="cat-item cat-item-1"><a href="#">Social Media</a></li>
+										<!-- <li class="cat-item cat-item-2"><a href="#">Portraits</a></li>
+										<li class="cat-item cat-item-3"><a href="#">Street</a></li> -->
+									</ul>
+								</li>
+								<!-- <li class="widget widget_archive">
+									<h5 class="widgettitle">Archives</h5>
+									<ul>
+										<li><a href="#">December 2025</a></li>
+										<li><a href="#"></a></li>
+									</ul>
+								</li> -->
+								<li class="widget widget_tag_cloud">
+									<h5 class="widgettitle">Tags</h5>
+									<div class="tagcloud"><a href="#">marketing</a> <a href="#">social media</a> <a href="#">funnels</a> 
+										<!-- <a href="#">family</a> <a href="#">fashion shoot</a> <a href="#">friend</a> <a href="#">hugs</a> <a href="#">light</a> <a href="#">mom</a> <a href="#">nature</a> <a href="#">neon</a> <a href="#">night</a> <a href="#">photo shoot</a> <a href="#">portrait</a> <a href="#">red</a> <a href="#">road</a> <a href="#">seaside</a> <a href="#">summer</a> <a href="#">sun</a> <a href="#">trip</a> <a href="#">vacation</a> <a href="#">wedding</a> <a href="#">woman</a> -->
+									</div>
+								</li>
+							</ul>
+						</aside>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /PAGE CONTENT -->
+		<?php include 'footer.php'; ?>
+		<!-- JS --> 
+		<script src='js/jquery.js'></script>
+		<script src='js/jquery-migrate.min.js'></script>
+		<script src='css/bootstrap/js/popper.js'></script>
+		<script src='css/bootstrap/js/bootstrap.js'></script>
+		<script src='js/easing.js'></script>
+		<script src='js/fitvids.js'></script>
+		<script src='js/owl-carousel/owl.carousel.js'></script>
+		<script src='js/isotope.js'></script>
+		<script src='js/simple-lightbox.js'></script>
+		<!-- MAIN JS -->
+		<script src='js/init.js'></script>
+		<!-- CONTAT FORM JS -->
+		<script src='js/jquery.form.min.js'></script>
+		<script src='js/contactform.js'></script>
+	</body>
+</html>
